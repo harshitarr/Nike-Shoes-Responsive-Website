ba6c4219084814e20f3a958ab5f8bb1726ebad56
Harshita <harshitarera@gmail.com>
2025-05-01 10:01:40 +0530

    010525-2

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,17 +1,37 @@
 import React from 'react'
 import headerlogo from '../assets/images/header-logo.svg'
 import hamburger from '../assets/icons/hamburger.svg'
+import { navLinks } from '../constants'
 
 const Nav = () => {
   return (
     <header className="padding-x py-8 absolute z-10 w-full">
-        <nav className="flex justify-between items-center max-container">
-            <a href="/">
-               <img src={headerlogo} alt="logo" width={130} height={29}/>
-            </a>
-        </nav>
-    </header>
-  )
-}
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerlogo} alt="logo" width={130} height={29} />
+        </a>
 
-export default Nav
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => {
+            
+            return (
+              <li key={item.label}>
+                <a
+                  href={item.href}
+                  className="font-montserrat leading-normal text-lg text-slate-gray"
+                >
+                  {item.label}
+                </a>
+              </li>
+            );
+          })}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} width={25} height={25}/>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Nav;
